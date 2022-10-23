4262855199aaeaf532d2aa6c878e05cae1e996c0
soufiane kayoub <soufiane.kayoub@gmail.com>
2022-10-23 20:19:33 +0200

    first exercise

--- src/com/company/Array.java
+++ src/com/company/Array.java
@@ -3,6 +3,7 @@ package com.company;
 public class Array {
     private int[] itemes;
     private int count;
+    private int largest;
 
     public Array(int legnth){
         itemes = new int[legnth];
@@ -22,7 +23,37 @@ public class Array {
     }
 
     public void RemoveAt(int index){
+        if(index < 1 || index >= count){
+            throw new IllegalArgumentException();
+        }
+        for(int i = index ; i<count; i++){
+            itemes[i] = itemes[i+1];
+        }
+        count--;
+    }
 
+    public int IndexOf(int item){
+        /*for (int i = 0; i<count; i++){
+            if(itemes[i] == item){
+                System.out.print(i + " ");
+            }
+        }*/
+
+        for(int  i = 0 ; i<count; i++){
+            if(itemes[i] == item)
+                return i;
+        }
+        return -1;
+    }
+
+    public int Max(){
+        largest = itemes[0];
+        for(int i = 0; i<count; i++){
+            if(largest < itemes[i+1]){
+                largest = itemes[i+1];
+            }
+        }
+        return largest;
     }
 
     public void print(){
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,14 @@ public class Main {
     public static void main(String[] args) {
 	    Array numbers = new Array(3);
 	    numbers.insert(10);
-	    numbers.insert(20);
+	    numbers.insert(40);
 	    numbers.insert(30);
-	    numbers.insert(40);numbers.insert(40);
+	    numbers.insert(20);
+		//numbers.RemoveAt(5);
+		//System.out.print(numbers.IndexOf(40));
 
-	    numbers.print();
+		System.out.print(numbers.Max());
+
+	    //numbers.print();
     }
 }
